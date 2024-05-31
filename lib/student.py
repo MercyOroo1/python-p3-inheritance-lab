812d1f6c91dd79946e12eb1155e14bd41a0994b5
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-05-31 14:35:42 +0300

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,16 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        knowledge = []
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self,string):
+        self.string = string
+        self.knowledge.append(string)
+        return self.knowledge
+
+calvin = Student("Calvin", "Klein")
+print(calvin.learn("ABC"))
+print(calvin.learn("alphabet"))
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,24 @@ from user import User
 import random
 
 class Teacher(User):
-
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
     def teach(self):
-        pass
+        self.knowledge = Teacher.knowledge
+        if self.knowledge :
+           random_index = random.randint(0,len(self.knowledge) - 1)
+           return self.knowledge [random_index]
+
+
+mary = Teacher ("Mary" ,"Doe")
+print (mary.last_name)
+print (mary.teach())
+print (mary.teach())
